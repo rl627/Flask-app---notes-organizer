1733a1208c0630ccdc4a54af75c0723c7d9c7b59
Ran Li <rl627@drexel.edu>
2021-02-07 12:49:00 -0500

    Imported and registered first route to app (__init__.py)

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,14 @@ def create_app():
     app = Flask(__name__) 
     app.config['SECRET_KEY'] = 'fsdfasdfasdfadscv' # secures cookies and session info tfor this app
 
+    # import blueprint
+    from .views import views
+    from .auth import auth
+
+    # register blueprint
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+
     return app
 
     
