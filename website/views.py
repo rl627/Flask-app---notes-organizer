ab6fafc8d65553f315a8e02e92929251695b5ab5
Ran Li <rl627@drexel.edu>
2021-02-07 13:18:18 -0500

    Fixed bug in views blueprint

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 # we will define that this file is the blueprint of our applciaiton; meaning it has a bunch of roots inside of it
-views = Blueprint('auth',  __name__)
+views = Blueprint('views',  __name__)
 
 # decorator: define a route: to define a view we use @blueprintname.route('path'); below '/' indicates home page
 # when ever we hit the route, we call the function
