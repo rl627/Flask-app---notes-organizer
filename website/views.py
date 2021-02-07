9f9905191b3acba1d039596cd355fe7078ac292a
Ran Li <rl627@drexel.edu>
2021-02-07 12:45:12 -0500

    Added first route (decorater + function) to views.py

--- website/views.py
+++ website/views.py
@@ -2,3 +2,9 @@ from flask import Blueprint
 
 # we will define that this file is the blueprint of our applciaiton; meaning it has a bunch of roots inside of it
 views = Blueprint('auth',  __name__)
+
+# decorator: define a route: to define a view we use @blueprintname.route('path'); below '/' indicates home page
+# when ever we hit the route, we call the function
+@views.route('/')  
+def home():
+    return "<h1>test</h1>"
